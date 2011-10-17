485fb1297476027ff85dcc188713543c0d014aa5
Ben Hearsum <bhearsum@mozilla.com>
2011-10-17 19:30:31 -0400

    Remove now-unneeded setup file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name = "AUS",
-    version = "0.1",
-    packages = find_packages(),
-    scripts = ['AUS-server.py', 'client.wsgi'],
-    install_requires = ['sqlalchemy==0.7.1', 'flask==0.7.2', 'simplejson']
-)
