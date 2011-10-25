ecddee6c40650cbaee9f567c31cef4813ef73869
Ben Hearsum <bhearsum@mozilla.com>
2011-10-25 15:24:22 -0400

    Look at REMOTE_USER instead of headers for username

--- auslib/web/views/base.py
+++ auslib/web/views/base.py
@@ -5,7 +5,7 @@ from flask import request, Response
 def requirelogin(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        username = request.headers.get('X-Remote-User')
+        username = request.environ.get('REMOTE_USER')
         if not username:
             return Response(status=401)
         return f(*args, changed_by=username, **kwargs)
