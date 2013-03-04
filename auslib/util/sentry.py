bcd757648fd4b4b02cdb3a73455bdc672a350768
Ben Hearsum <bhearsum@mozilla.com>
2013-03-04 09:57:45 -0500

    Fix sanitizer

--- auslib/util/sentry.py
+++ auslib/util/sentry.py
@@ -1,11 +1,12 @@
 from raven.processors import Processor
 
 class SanitizeHeadersProcessor(Processor):
-    def __init__(self, sanitizeHeaders=['Authorization']):
-        self.sanitizeHeaders = sanitizeHeaders
+    sanitizeHeaders = ['Authorization']
 
-    def filter_http(self, data):
+    def process(self, data):
+        data = data['sentry.interfaces.Http']
         if 'headers' in data:
             for header in self.sanitizeHeaders:
                 if header in data['headers']:
                     data['headers'][header] = 'REDACTED'
+        return data
