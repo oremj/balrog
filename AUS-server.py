4c25d72b7a5424ccfd3a85cf81eb31176cc4350e
Ben Hearsum <bhearsum@mozilla.com>
2013-03-28 15:19:04 -0400

    Improve AUS Server

--- AUS-server.py
+++ AUS-server.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     )
     parser.add_option("-d", "--db", dest="db", help="database to use, relative to inputdir")
     parser.add_option("-p", "--port", dest="port", type="int", help="port for server")
+    parser.add_option("--host", dest="host", default='127.0.0.1', help="host to listen on. for example, 0.0.0.0 binds on all interfaces.")
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
         help="Verbose output")
     options, args = parser.parse_args()
@@ -24,8 +25,11 @@ if __name__ == "__main__":
     logging.basicConfig(level=log_level, format=log_format)
 
     AUS.setDb(options.db)
-    AUS.db.create()
+    try:
+        db.create()
+    except DatabaseAlreadyControlledError:
+        pass
 
     app.config['SECRET_KEY'] = 'abc123'
     app.config['DEBUG'] = True
-    app.run(port=options.port)
+    app.run(port=options.port, host=options.host)
