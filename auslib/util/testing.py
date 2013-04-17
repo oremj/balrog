5f973e9dd9864064b6541dadb1f5c2ca02626470
Ben Hearsum <bhearsum@mozilla.com>
2013-04-17 13:32:04 -0400

    Use internal retries.

--- auslib/util/testing.py
+++ auslib/util/testing.py
@@ -2,11 +2,19 @@ import difflib
 
 import requests
 
+from mozilla_buildtools.retry import retry
+
 
 def compare_snippets(url1, url2, retries=3, timeout=10, diff=True):
-    cfg = {'max_retries': retries}
-    xml1 = requests.get(url1, timeout=timeout, config=cfg).content.splitlines()
-    xml2 = requests.get(url2, timeout=timeout, config=cfg).content.splitlines()
+    cfg = {'danger_mode': True}
+    xml1 = retry(requests.get, sleeptime=5, attempts=retries, args=(url1,),
+                 retry_exceptions=(requests.HTTPError, requests.ConnectionError),
+                 kwargs={'timeout': timeout, 'config': cfg})
+    xml1 = xml1.content.splitlines()
+    xml2 = retry(requests.get, sleeptime=5, attempts=retries, args=(url2,),
+                 retry_exceptions=(requests.HTTPError, requests.ConnectionError),
+                 kwargs={'timeout': timeout, 'config': cfg})
+    xml2 = xml2.content.splitlines()
     ret = [url1, xml1, url2, xml2]
     if xml1 != xml2:
         if diff:
