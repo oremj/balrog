6874e5a8325ff61db4476c43b8fc0c62a5766d12
Ben Hearsum <bhearsum@mozilla.com>
2014-01-24 16:33:40 -0500

    WIP

--- auslib/web/base.py
+++ auslib/web/base.py
@@ -18,14 +18,11 @@ def fourohfour(error):
     response.mimetype = 'text/xml'
     return response
 
-@app.errorhandler(500)
-def isa(error):
-    log.error("Caught ISE 500 error.")
-    log.debug("Balrog version is: %s", version)
-    log.debug("Request path is: %s", request.path)
-    log.debug("Request environment is: %s", request.environ)
-    log.debug("Request headers are: %s", request.headers)
-    return error
+@app.errorhandler(Exception)
+def generic(error):
+    response = make_response('<?xml version="1.0"?>\n<updates>\n</updates>')
+    response.mimetype = 'text/xml'
+    return response
 
 app.add_url_rule(
     '/update/2/<product>/<version>/<buildID>/<buildTarget>/<locale>/<channel>/<osVersion>/update.xml',
