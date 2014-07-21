e5bf5430d95a672115d19bddaef7dfe19542e839
Ben Hearsum <bhearsum@mozilla.com>
2014-07-21 11:47:00 -0400

    Add comment about database wrapper object.

--- auslib/__init__.py
+++ auslib/__init__.py
@@ -1,12 +1,18 @@
 version = "0.5"
 
-# TODO: comment about why this is needed
+# auslib is a library that contains two different webapps. Both of them share
+# a single database model, and some code (release blobs, for example), need
+# to have access to the database to do their jobs. Because of this, it's
+# convenient to have the database object accessible in single place for both
+# apps, which is why the "dbo" variable below exists. This DbWrapper class
+# exists solely to defer the AUSDatabase object import to work around circular
+# dependency issues that would occur if it were imported at parse time instead
+# of runtime.
 class DbWrapper(object):
     def __init__(self):
         self.db = None
 
     def setDb(self, dburi):
-        # This import is deferred to avoid circular depedency issues.
         from auslib.db import AUSDatabase
         self.db = AUSDatabase(dburi)
 
