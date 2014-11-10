d51c6f02762b18c2bb4ef9ab762892b80b3f06d4
Ben Hearsum <bhearsum@mozilla.com>
2014-11-10 15:58:01 -0500

    Fix comment.

--- auslib/admin/base.py
+++ auslib/admin/base.py
@@ -41,24 +41,24 @@ def add_security_headers(response):
 Compress(app)
 
 # Endpoints required for the Balrog 2.0 UI.
-app.add_url_rule('/api/csrf_token', view_func=CSRFView.as_view('api_csrf'))
-app.add_url_rule('/api/users', view_func=UsersView.as_view('api_users'))
-app.add_url_rule('/api/users/<username>/permissions', view_func=PermissionsView.as_view('api_user_permissions'))
-app.add_url_rule('/api/users/<username>/permissions/<path:permission>', view_func=SpecificPermissionView.as_view('api_specific_permission'))
-# Some permissions may start with a slash, and the <path> converter won't match them, so we need an extra rule to cope.
-app.add_url_rule('/api/users/<username>/permissions//<path:permission>', view_func=SpecificPermissionView.as_view('api_specific_permission2'))
-app.add_url_rule('/api/rules', view_func=RulesAPIView.as_view('api_rules'))
-app.add_url_rule('/api/rules/<rule_id>', view_func=SingleRuleView.as_view('api_rule'))
-app.add_url_rule('/api/rules/<rule_id>/revisions', view_func=RuleHistoryAPIView.as_view('api_rules_revisions'))
-app.add_url_rule('/api/releases', view_func=ReleasesAPIView.as_view('api_releases'))
-app.add_url_rule('/api/releases/<release>', view_func=SingleReleaseView.as_view('api_releases_revision'))
-app.add_url_rule('/api/releases/<release>/builds/<platform>/<locale>', view_func=SingleLocaleView.as_view('api_single_locale'))
-app.add_url_rule('/api/releases/<release>/revisions', view_func=ReleaseHistoryView.as_view('api_release_revisions'))
-app.add_url_rule('/api/history/diff/<type_>/<change_id>/<field>', view_func=DiffView.as_view('api_diff'))
-app.add_url_rule('/api/history/view/<type_>/<change_id>/<field>', view_func=FieldView.as_view('api_field'))
+app.add_url_rule("/api/csrf_token", view_func=CSRFView.as_view("api_csrf"))
+app.add_url_rule("/api/users", view_func=UsersView.as_view("api_users"))
+app.add_url_rule("/api/users/<username>/permissions", view_func=PermissionsView.as_view("api_user_permissions"))
+app.add_url_rule("/api/users/<username>/permissions/<path:permission>", view_func=SpecificPermissionView.as_view("api_specific_permission"))
+# Some permissions may start with a slash, and the <path> converter won"t match them, so we need an extra rule to cope.
+app.add_url_rule("/api/users/<username>/permissions//<path:permission>", view_func=SpecificPermissionView.as_view("api_specific_permission2"))
+app.add_url_rule("/api/rules", view_func=RulesAPIView.as_view("api_rules"))
+app.add_url_rule("/api/rules/<rule_id>", view_func=SingleRuleView.as_view("api_rule"))
+app.add_url_rule("/api/rules/<rule_id>/revisions", view_func=RuleHistoryAPIView.as_view("api_rules_revisions"))
+app.add_url_rule("/api/releases", view_func=ReleasesAPIView.as_view("api_releases"))
+app.add_url_rule("/api/releases/<release>", view_func=SingleReleaseView.as_view("api_releases_revision"))
+app.add_url_rule("/api/releases/<release>/builds/<platform>/<locale>", view_func=SingleLocaleView.as_view("api_single_locale"))
+app.add_url_rule("/api/releases/<release>/revisions", view_func=ReleaseHistoryView.as_view("api_release_revisions"))
+app.add_url_rule("/api/history/diff/<type_>/<change_id>/<field>", view_func=DiffView.as_view("api_diff"))
+app.add_url_rule("/api/history/view/<type_>/<change_id>/<field>", view_func=FieldView.as_view("api_field"))
 
 
-# Deprecated endpoints. These can be removed when the old UI is shut off.
+# Deprecated endpoints. These can be removed when the old UI is shut off _and_ submitter tools use the new "/api" endpoints.
 app.add_url_rule('/csrf_token', view_func=CSRFView.as_view('csrf'))
 app.add_url_rule('/users', view_func=UsersView.as_view('users'))
 app.add_url_rule('/users/<username>/permissions', view_func=PermissionsView.as_view('permissions'))
