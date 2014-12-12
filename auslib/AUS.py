f0a05176ce89940ea65aa8b9814f79db1dc482d7
Ben Hearsum <bhearsum@mozilla.com>
2014-12-12 10:26:01 -0500

    Revert rule caching because it's moving to another branch.

--- auslib/AUS.py
+++ auslib/AUS.py
@@ -3,7 +3,7 @@ from urlparse import urlparse
 
 import logging
 
-from auslib.global_state import cache, dbo
+from auslib import dbo
 from auslib.log import cef_event, CEF_ALERT
 
 
@@ -46,39 +46,21 @@ class AUS:
         self.log = logging.getLogger(self.__class__.__name__)
 
     def evaluateRules(self, updateQuery):
-        # The cache key needs to be hashable, so we can't use the updateQuery
-        # object. Sorting the values of updateQuery makes sure we don't have
-        # cache misses if the natural "order" of the updateQuery dict changes
-        # from request to request
-        # This is a complex statement, so here's an unpacked explanation
-        # from the inner most portion to outer:
-        # * Start with all of the values in the updateQuery
-        # * Sort them, for consistency across requests
-        # * Coerce them to strings, because some may not be
-        # * Join them into a single string, delimited by "-"
-        #cache_key = "-".join([str(v) for v in sorted(updateQuery.values())])
-        #cached = cache.get("evaluated_rules", cache_key)
-        cached = False
-        if cached:
-            self.log.debug("Using evaluated rules from cache.")
-            rule = cached
-        else:
-            self.log.debug("Looking for rules that apply to:")
-            self.log.debug(updateQuery)
-            rules = dbo.rules.getRulesMatchingQuery(
-                updateQuery,
-                fallbackChannel=getFallbackChannel(updateQuery['channel'])
-            )
+        self.log.debug("Looking for rules that apply to:")
+        self.log.debug(updateQuery)
+        rules = dbo.rules.getRulesMatchingQuery(
+            updateQuery,
+            fallbackChannel=getFallbackChannel(updateQuery['channel'])
+        )
 
-            ### XXX throw any N->N update rules and keep the highest priority remaining one
-            if len(rules) < 1:
-                return None, None
+        ### XXX throw any N->N update rules and keep the highest priority remaining one
+        if len(rules) < 1:
+            return None, None
 
-            rules = sorted(rules,key=lambda rule: rule['priority'], reverse=True)
-            rule = rules[0]
-            #cache.put("evaluated_rules", cache_key, rule)
-
+        rules = sorted(rules,key=lambda rule: rule['priority'], reverse=True)
+        rule = rules[0]
         self.log.debug("Matching rule: %s" % rule)
+
         # There's a few cases where we have a matching rule but don't want
         # to serve an update:
         # 1) No mapping.
@@ -99,9 +81,10 @@ class AUS:
         # 3) Incoming release is older than the one in the mapping, defined as one of:
         #    * version decreases
         #    * version is the same and buildID doesn't increase
-        blob = dbo.releases.getReleaseBlob(name=rule["mapping"])
+        release = dbo.releases.getReleases(name=rule['mapping'], limit=1)[0]
+        blob = release['data']
         if not blob.shouldServeUpdate(updateQuery):
             return None, None
 
-        self.log.debug("Returning release %s", blob["name"])
+        self.log.debug("Returning release %s", release['name'])
         return blob, rule['update_type']
