bbb9faf7555500add3344d1230323bbe438aee39
Ben Hearsum <bhearsum@mozilla.com>
2019-04-29 10:04:00 -0400

    Fix hash checking and implement skip patterns in upload script.

--- scripts/releases-history-to-gcs.py
+++ scripts/releases-history-to-gcs.py
@@ -9,6 +9,8 @@ import requests
 
 from google.cloud import storage
 
+skip_patterns = ("-nightly-",)
+
 balrog_api = sys.argv[1]
 
 releases = {}
@@ -21,16 +23,26 @@ bucket = client.get_bucket(sys.argv[2])
 
 uploads = defaultdict(int)
 
-for r in ("XP-Vista-Desupport",):#releases:
+for n, r in enumerate(releases):
+    print("Processing {}, {}% complete".format(r, int(n / len(releases) * 100)))
+    if any(pat in r for pat in skip_patterns):
+        print("  Skipping because it matches a skip pattern")
+    n += 1
+    if n == 10:
+        break
     for rev in requests.get("{}/releases/{}/revisions".format(balrog_api, r)).json()["revisions"]:
         old_version = requests.get("{}/history/view/release/{}/data".format(balrog_api, rev["change_id"])).text
         old_version_hash = hashlib.md5(old_version.encode("ascii")).digest()
-        current_blob_hash = base64.b64decode(bucket.get_blob("{}/{}-{}-{}.json".format(r, rev["data_version"], rev["timestamp"], rev["changed_by"])).md5_hash)
+        current_blob = bucket.get_blob("{}/{}-{}-{}.json".format(r, rev["data_version"], rev["timestamp"], rev["changed_by"]))
+        current_blob_hash = None
+        if current_blob:
+            current_blob_hash = base64.b64decode(current_blob.md5_hash)
         if old_version_hash != current_blob_hash:
+            print("  Uploading data version {}".format(rev["data_version"]))
             blob = bucket.blob("{}/{}-{}-{}.json".format(r, rev["data_version"], rev["timestamp"], rev["changed_by"]))
             blob.upload_from_string(old_version, content_type="application/json")
         else:
-            print("Skipping {} data version {}".format(r, rev["data_version"]))
+            print("  Skipping data version {} because its md5 matches".format(rev["data_version"]))
         uploads[r] += 1
 
 for r in releases:
